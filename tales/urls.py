6303c05de8d614bb5ef3a8cf986729fc697ad4c5
vishnuramv <vishnu.v1902@gmail.com>
2021-08-23 15:07:55 +0530

    Fix: slashes

--- tales/urls.py
+++ tales/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include, re_path
 from django.conf.urls.static import static
 from django.conf import settings
 # from rest_framework.documentation import include_docs_urls
+from django_comments_xtd.views import XtdCommentListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
